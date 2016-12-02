c32dc35caa676577cc2f1cbcfb2b035ab1bf1fd6
KeerthiTejaN <keerthi.nuthi@gmail.com>
2016-12-02 14:17:55 -0500

    Added GeoCoordinates Class

---
+++ app/src/main/java/com/starters/medion/model/GeoCoordinates.java
@@ -0,0 +1,35 @@
+package com.starters.medion.model;
+
+/**
+ * Created by KeerthiTejaNuthi on 12/2/16.
+ */
+
+public class GeoCoordinates {
+    private double latitude;
+    private double longitude;
+
+    public GeoCoordinates(){
+
+    }
+    
+    public GeoCoordinates(double latitute, double longitude){
+        this.latitude = latitute;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
