389bed29a2d13349e5b9f6153b645b87b4278367
KeerthiTejaN <keerthi.nuthi@gmail.com>
2016-12-09 20:07:15 -0500

    Adding a POJO

---
+++ app/src/main/java/com/starters/medion/model/Eid.java
@@ -0,0 +1,17 @@
+package com.starters.medion.model;
+
+/**
+ * Created by KeerthiTejaNuthi on 12/9/16.
+ */
+
+public class Eid {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
